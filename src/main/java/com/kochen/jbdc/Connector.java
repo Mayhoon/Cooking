8369a72d594cabd2c72e75173bd0a843e56d94bb
Mayhoon <fell2ratte@t-online.de>
2021-03-26 01:17:30 +0100

    Changed db port

--- src/main/java/com/kochen/jbdc/Connector.java
+++ src/main/java/com/kochen/jbdc/Connector.java
@@ -9,7 +9,7 @@ public class Connector {
 
     public static Statement connect() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        String url = "jdbc:mysql://localhost:3306/cooking";
+        String url = "jdbc:mysql://localhost:3307/cooking";
         String username = "root";
         String password = "root";
         Connection connection = DriverManager.getConnection(url, username, password);
